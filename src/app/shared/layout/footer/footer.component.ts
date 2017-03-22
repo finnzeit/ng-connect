3c84e0de875d3e31d85e927e14d204d95db02614
finnzeit <finnzeit@gmail.com>
2017-03-22 16:06:05 +0700

    Fix typo

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'layout-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
